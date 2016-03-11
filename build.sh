50e1cab326731c9f3662fbaca740ce24f2dbb685
Kevin Carter <kevin.carter@rackspace.com>
2016-03-10 19:51:04 -0600

    Updated scripts and process for better integration
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ fi
 
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 
-apt-get update && apt-get install -y qemu-kvm libvirt-bin virtinst bridge-utils virt-manager
+apt-get update && apt-get install -y qemu-kvm libvirt-bin virtinst bridge-utils virt-manager lvm2
 
 virsh net-autostart default --disable
 virsh net-destroy default
@@ -70,10 +70,13 @@ iptables -w -t mangle \
             --checksum-fill
 
 # Create kvm storage pool
-parted --script /dev/sdb mklabel gpt
-parted --script /dev/sdb mkpart kvm ext4 0% 100%
-mkfs.ext4 /dev/sdb1
-echo "$(blkid | awk '/\/dev\/sdb1/ {print $2}') /var/lib/libvirt/images ext4 errors=remount-ro 0 1" | tee -a /etc/fstab
+DATA_DISK_DEVICE=$(lsblk -brndo NAME,TYPE,FSTYPE,RO,SIZE | awk '/d[b-z]+ disk +0/{ if ($4>m){m=$4; d=$1}}; END{print d}')
+parted --script /dev/${DATA_DISK_DEVICE} mklabel gpt
+parted --align optimal --script /dev/${DATA_DISK_DEVICE} mkpart kvm ext4 0% 100%
+mkfs.ext4 /dev/${DATA_DISK_DEVICE}1
+if ! grep -qw "^/dev/${DATA_DISK_DEVICE}1" /etc/fstab; then
+  echo "/dev/${DATA_DISK_DEVICE}1 ${BOOTSTRAP_AIO_DIR} ext4 defaults 0 0" >> /etc/fstab
+fi
 mount -a
 
 # Install cobbler
@@ -84,12 +87,6 @@ apt-get update && apt-get -y install cobbler dhcp3-server debmirror isc-dhcp-ser
 # Move Cobbler Apache config to the right place
 cp /etc/apache2/conf.d/cobbler.conf /etc/apache2/conf-available/
 cp /etc/apache2/conf.d/cobbler_web.conf /etc/apache2/conf-available/
-
-# Fix Apache conf to match 2.4 configuration
-sed -i "/Order allow,deny/d" /etc/apache2/conf-enabled/cobbler*.conf
-sed -i "s/Allow from all/Require all granted/" /etc/apache2/conf-enabled/cobbler*.conf
-sed -i "s/^Listen 80/Listen 5150/" /etc/apache2/ports.conf
-sed -i "s/\:80/\:5150/" /etc/apache2/sites-available/000-default.conf
 
 # Enable the above config
 a2enconf cobbler cobbler_web
@@ -105,6 +102,12 @@ sed -i 's/^next_server\:.*/next_server\: 10.0.0.200/g' /etc/cobbler/settings
 sed -i 's/^server\:.*/server\: 10.0.0.200/g' /etc/cobbler/settings
 sed -i 's/^http_port\:.*/http_port\: 5150/g' /etc/cobbler/settings
 sed -i 's/^INTERFACES.*/INTERFACES="br-dhcp"/g' /etc/default/isc-dhcp-server
+
+# Fix Apache conf to match 2.4 configuration
+sed -i "/Order allow,deny/d" /etc/apache2/conf-enabled/cobbler*.conf
+sed -i "s/Allow from all/Require all granted/g" /etc/apache2/conf-enabled/cobbler*.conf
+sed -i "s/^Listen 80/Listen 5150/g" /etc/apache2/ports.conf
+sed -i "s/\:80/\:5150/g" /etc/apache2/sites-available/000-default.conf
 
 # Fix TFTP server arguments in cobbler template to enable it to work on Ubuntu
 sed -i "s/server_args .*/server_args             = -s \$args/" /etc/cobbler/tftpd.template
@@ -129,13 +132,14 @@ DEFAULT_NETWORK="eth0"
 #  when templated replace \$ with $ and \\ with \
 cp templates/ubuntu-server-14.04-unattended-cobbler.seed /var/lib/cobbler/kickstarts/ubuntu-server-14.04-unattended-cobbler.seed
 sed -i "s/__DEVICE_NAME__/${DEVICE_NAME}/g" /var/lib/cobbler/kickstarts/ubuntu-server-14.04-unattended-cobbler.seed
-sed -i "s/__SSHKEY__/${SSHKEY}/g" /var/lib/cobbler/kickstarts/ubuntu-server-14.04-unattended-cobbler.seed
+sed -i "s|__SSHKEY__|${SSHKEY}|g" /var/lib/cobbler/kickstarts/ubuntu-server-14.04-unattended-cobbler.seed
 sed -i "s/__DEFAULT_NETWORK__/${DEFAULT_NETWORK}/g" /var/lib/cobbler/kickstarts/ubuntu-server-14.04-unattended-cobbler.seed
 
 # Restart services again and configure autostart
 service cobblerd restart
 service apache2 restart
-service xinetd restart
+service xinetd stop
+service xinetd start
 update-rc.d cobblerd defaults
 
 # Get ubuntu server image
@@ -145,18 +149,23 @@ pushd /var/cache/iso
 popd
 
 # import cobbler image
-mkdir -p /mnt/iso
-mount -o loop /var/cache/iso/ubuntu-14.04.4-server-amd64.iso /mnt/iso
-cobbler import --name=ubuntu-14.04.4-server-amd64 --path=/mnt/iso
-umount /mnt/iso
+if ! cobbler distro list | grep -qw "ubuntu-14.04.4-server-x86_64"; then
+  mkdir -p /mnt/iso
+  mount -o loop /var/cache/iso/ubuntu-14.04.4-server-amd64.iso /mnt/iso
+  cobbler import --name=ubuntu-14.04.4-server-amd64 --path=/mnt/iso
+  umount /mnt/iso
+fi
 
-cobbler profile add \
-  --name ubuntu-14.04.4-server-unattended \
-  --distro ubuntu-14.04.4-server-x86_64 \
-  --kickstart /var/lib/cobbler/kickstarts/ubuntu-server-14.04-unattended-cobbler.seed
+# Create cobbler profile
+if ! cobbler profile list | grep -qw "ubuntu-14.04.4-server-unattended"; then
+  cobbler profile add \
+    --name ubuntu-14.04.4-server-unattended \
+    --distro ubuntu-14.04.4-server-x86_64 \
+    --kickstart /var/lib/cobbler/kickstarts/ubuntu-server-14.04-unattended-cobbler.seed
 
-# sync cobbler
-cobbler sync
+  # sync cobbler
+  cobbler sync
+fi
 
 # Get Loaders
 cobbler get-loaders
@@ -164,82 +173,107 @@ cobbler get-loaders
 # Update Cobbler Signatures
 cobbler signature update
 
-# Create the cobbler system
-count=0
-for node in infra1 infra2 infra3 logging1 compute1 compute2 swift1 swift2 storage1; do
-  node_count="$((count++))"
-  cobbler system add \
-    --name=${node} \
-    --profile=ubuntu-14.04.4-server-unattended \
-    --hostname=${node}.openstackci.local \
-    --kopts="interface=${DEFAULT_NETWORK}" \
-    --interface=${DEFAULT_NETWORK} \
-    --mac="52:54:00:bd:81:d${node_count}" \
-    --ip-address="10.0.0.10${node_count}" \
-    --subnet=255.255.255.0 \
-    --gateway=10.0.0.200 \
-    --name-servers=8.8.8.8 8.8.4.4 \
-    --static=1
-done
 
-# Create the libvirt networks used for the Host VMs
-for network in br-dhcp br-mgmt br-vxlan br-storage br-vlan; do
-  sed "s/__NETWORK__/${network}/g" templates/libvirt-network.xml > /etc/libvirt/qemu/networks/${network}.xml
-  virsh net-define --file /etc/libvirt/qemu/networks/${network}.xml
-  virsh net-create --file /etc/libvirt/qemu/networks/${network}.xml
-  virsh net-autostart ${network}
-done
+function get_host_type () {
+python <<EOL
+import json
+with open('hosts.json') as f:
+    x = json.loads(f.read())
+for k, v in x.get("$1").items():
+    print('%s:%s' % (k, v))
+EOL
+}
 
-count=0
-for node in infra1 infra2 infra3 logging1 compute1 compute2 swift1 swift2 storage1; do
-  node_count="$((count++))"
-  qemu-img create -f qcow2 /var/lib/libvirt/images/${node}.openstackci.local.img 252G
-  cp templates/vmnode.openstackci.local.xml /etc/libvirt/qemu/${node}.openstackci.local.xml
-  sed -i "s/__NODE__/${node}/g" /etc/libvirt/qemu/${node}.openstackci.local.xml
-  sed -i "s/__COUNT__/${count}/g" /etc/libvirt/qemu/${node}.openstackci.local.xml
-  virsh define /etc/libvirt/qemu/${node}.openstackci.local.xml
-  virsh create /etc/libvirt/qemu/${node}.openstackci.local.xml
-  cp templates/vmnode.openstackci.local-bridges.cfg /opt/osa-${node}.openstackci.local-bridges.cfg
-  sed -i "s/__COUNT__/${count}/g" /opt/osa-${node}.openstackci.local-bridges.cfg
-done
+function get_all_hosts () {
+python <<EOL
+import json
+with open('hosts.json') as f:
+    x = json.loads(f.read())
+for i in x.values():
+    for k, v in i.items():
+      print('%s:%s' % (k, v))
+EOL
+}
 
 function wait_ssh() {
-count=0
 echo "Waiting for all nodes to become available. This can take around 10 min"
-for node in infra1 infra2 infra3 logging1 compute1 compute2 swift1 swift2 storage1; do
-node_count="$((count++))"
-    echo "Waiting for node: ${node}"
-    ssh -q -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=10 10.0.0.10${node_count} exit > /dev/null
+for node in $(get_all_hosts); do
+    echo "Waiting for node: ${node%%":"*} on 10.0.0.${node#*":"}"
+    ssh -q -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=10 10.0.0.${node#*":"} exit > /dev/null
     while test $? -gt 0; do
       sleep 15
-      ssh -q -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=10 10.0.0.10${node_count} exit > /dev/null
+      ssh -q -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=10 10.0.0.${node#*":"} exit > /dev/null
     done
 done
 }
 
+# Create the cobbler systems
+for node in $(get_all_hosts); do
+  if ! cobbler system list | grep -qw "${node%%":"*}"; then
+    cobbler system add \
+      --name=${node%%":"*} \
+      --profile=ubuntu-14.04.4-server-unattended \
+      --hostname=${node%%":"*}.openstackci.local \
+      --kopts="interface=${DEFAULT_NETWORK}" \
+      --interface=${DEFAULT_NETWORK} \
+      --mac="52:54:00:bd:81:${node:(-2)}" \
+      --ip-address="10.0.0.${node#*":"}" \
+      --subnet=255.255.255.0 \
+      --gateway=10.0.0.200 \
+      --name-servers=8.8.8.8 8.8.4.4 \
+      --static=1
+  fi
+done
+
+# sync cobbler
+cobbler sync
+
+# Create the libvirt networks used for the Host VMs
+for network in br-dhcp br-mgmt br-vxlan br-storage br-vlan; do
+  if ! virsh net-list |  grep -qw "${network}"; then
+    sed "s/__NETWORK__/${network}/g" templates/libvirt-network.xml > /etc/libvirt/qemu/networks/${network}.xml
+    virsh net-define --file /etc/libvirt/qemu/networks/${network}.xml
+    virsh net-create --file /etc/libvirt/qemu/networks/${network}.xml
+    virsh net-autostart ${network}
+  fi
+done
+
+# Create the VM root disk then define and start the VMs.
+#  !!!THIS TASK WILL DESTROY ALL OF THE ROOT DISKS IF THEY ALREADY EXIST!!!
+for node in $(get_all_hosts); do
+  qemu-img create -f qcow2 /var/lib/libvirt/images/${node%%":"*}.openstackci.local.img 252G
+  cp templates/vmnode.openstackci.local.xml /etc/libvirt/qemu/${node%%":"*}.openstackci.local.xml
+  sed -i "s/__NODE__/${node%%":"*}/g" /etc/libvirt/qemu/${node%%":"*}.openstackci.local.xml
+  sed -i "s/__COUNT__/${node:(-2)}/g" /etc/libvirt/qemu/${node%%":"*}.openstackci.local.xml
+  virsh define /etc/libvirt/qemu/${node%%":"*}.openstackci.local.xml || true
+  virsh create /etc/libvirt/qemu/${node%%":"*}.openstackci.local.xml
+  cp templates/vmnode.openstackci.local-bridges.cfg /opt/osa-${node%%":"*}.openstackci.local-bridges.cfg
+  sed -i "s/__COUNT__/${node#*":"}/g" /opt/osa-${node%%":"*}.openstackci.local-bridges.cfg
+done
+
+# Wait here for all nodes to be booted and ready with SSH
 wait_ssh
 
-count=0
-for node in infra1 infra2 infra3 logging1 compute1 compute2 swift1 swift2 storage1; do
-node_count="$((count++))"
-echo "Setup node bridge configs: ${node}"
-scp -o StrictHostKeyChecking=no /opt/osa-${node}.openstackci.local-bridges.cfg 10.0.0.10${node_count}:/etc/network/interfaces.d/osa-${node}.openstackci.local-bridges.cfg
-ssh -q -o StrictHostKeyChecking=no 10.0.0.10${node_count} <<EOF
+# Do the basic host setup for all nodes
+for node in $(get_all_hosts); do
+scp -o StrictHostKeyChecking=no /opt/osa-${node%%":"*}.openstackci.local-bridges.cfg 10.0.0.${node#*":"}:/etc/network/interfaces.d/osa-${node%%":"*}.openstackci.local-bridges.cfg
+ssh -q -o StrictHostKeyChecking=no 10.0.0.${node#*":"} <<EOF
 if ! grep "^source.*cfg$" /etc/network/interfaces; then
 echo 'source /etc/network/interfaces.d/*.cfg' | tee -a /etc/network/interfaces
 fi
 umount /deleteme || true
 echo y | lvremove  /dev/lxc/deleteme00 || true
 sed -i 's/^\/dev\/mapper\/lxc-deleteme00.*//g' /etc/fstab
-for i in br-dhcp br-mgmt br-vlan br-storage br-vxlan; do
-  ifup $i;
+for i in br-mgmt br-vlan br-storage br-vxlan; do
+  ifup \$i;
+  echo "\$i done"
 done
 EOF
 done
 
 # Infra storage setup
-for node in 10.0.0.100 10.0.0.101 10.0.0.102; do
-ssh -q -o StrictHostKeyChecking=no ${node} <<EOF
+for node in $(get_host_type infra); do
+ssh -q -o StrictHostKeyChecking=no 10.0.0.${node#*":"} <<EOF
 umount /var/lib/nova
 echo y | lvremove  /dev/lxc/nova00 || true
 sed -i 's/^\/dev\/mapper\/lxc-nova00.*//g' /etc/fstab
@@ -248,8 +282,8 @@ EOF
 done
 
 # Logging storage setup
-for node in 10.0.0.103; do
-ssh -q -o StrictHostKeyChecking=no ${node} <<EOF
+for node in $(get_host_type logging); do
+ssh -q -o StrictHostKeyChecking=no 10.0.0.${node#*":"} <<EOF
 umount /var/lib/nova
 echo y | lvremove  /dev/lxc/nova00 || true
 sed -i 's/^\/dev\/mapper\/lxc-nova00.*//g' /etc/fstab
@@ -258,8 +292,8 @@ EOF
 done
 
 # swift storage setup
-for node in 10.0.0.106 10.0.0.107; do
-ssh -q -o StrictHostKeyChecking=no ${node} <<EOF
+for node in $(get_host_type swift); do
+ssh -q -o StrictHostKeyChecking=no 10.0.0.${node#*":"} <<EOF
 umount /var/lib/nova
 echo y | lvremove  /dev/lxc/nova00 || true
 sed -i 's/^\/dev\/mapper\/lxc-nova00.*//g' /etc/fstab
@@ -271,15 +305,14 @@ mkdir -p /src/\${disk}
 mount /dev/lxc/\${disk} /srv/\${disk}
 echo "/dev/mapper/lxc-\${disk} /srv/\${disk} xfs defaults 0 0" | tee -a /etc/fstab
 done
-
 EOF
 done
 
 # Storage storage setup
-for node in 10.0.0.108; do
-ssh -q -o StrictHostKeyChecking=no ${node} <<EOF
+for node in $(get_host_type storage); do
+ssh -q -o StrictHostKeyChecking=no 10.0.0.${node#*":"} <<EOF
 umount /var/lib/nova
-echo y | lvremove  /dev/lxc/nova00 || true
+echo y | lvremove /dev/lxc/nova00 || true
 sed -i 's/^\/dev\/mapper\/lxc-nova00.*//g' /etc/fstab
 lvcreate --name cinder -l 100%FREE lxc
 vgcreate cinder-volumes /dev/lxc/cinder
